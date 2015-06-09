dc225af809cbd339f949b1f8ebaf61026c5ce681
Stephen <e2stephen@cox.net>
2015-06-08 17:14:34 -0700

    bootstrap installeed on initial view

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -4,45 +4,25 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 <html lang="en">
 <head>
 	<meta charset="utf-8">
-	<title>Welcome to CodeIgniter</title>
-
-	<style type="text/css">
-
-	::selection { background-color: #E13300; color: white; }
-	::-moz-selection { background-color: #E13300; color: white; }
-
-	body {
-		background-color: #fff;
-		margin: 40px;
-		font: 13px/20px normal Helvetica, Arial, sans-serif;
-		color: #4F5155;
-	}
-
-	a {
-		color: #003399;
-		background-color: transparent;
-		font-weight: normal;
-	}
-
-	#body {
-		margin: 0 15px 0 15px;
-	}
-
-	#container {
-		margin: 10px;
-		border: 1px solid #D0D0D0;
-		box-shadow: 0 0 8px #D0D0D0;
-	}
-	</style>
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>User Dashboard Login	</title>
+	<link href="../../assets/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body>
-
 <div id="container">
-
+	<div id="header">
+	</div>
 	<div id="body">
-
+		<p>hello world</p>
 	</div>
 </div>
+
+
+<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+<!-- Include all compiled plugins (below), or include individual files as needed -->
+<script src="../../assets/js/bootstrap.min.js"></script>
 
 </body>
 </html>
